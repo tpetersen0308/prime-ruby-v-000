da4a104b4aa01b5bac24ed5e55ea20dd11c84d7c
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-27 22:03:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(n)
   whole_numbers = (2..(n - 1)).to_a
-  return false if whole_numbers.any? { |number| n % number == 0 } || n < 0
+  return false if whole_numbers.any? { |number| n % number == 0 } || n <= 0
   return true
 end
